f6f0c0b17cc952bc9536773cb6da61d23276be92
surya-prabhat <suryaprabhat2002@gmail.com>
2025-07-12 16:36:01 +0530

    final commit-  for now

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const createDessert = async () => {
             <div class="product-info">
               <p class="category">${i.category}</p>
               <p class="dessert-name" id="${i.id}">${i.name}</p>
-              <span class="dessert-price">$<span>${i.price}</span></span>
+              <span class="dessert-price">$<span class="d-price">${i.price}</span></span>
             </div>
           </div>`
   }
@@ -46,12 +46,13 @@ const createDessert = async () => {
   cartWithItem.classList.add("cartWithItem-off")
 
   const addItemMultiple = document.querySelector(".add-item-multiple")
+  console.dir(addItemMultiple)
 
 
 
   addItemMultiple.innerHTML = ""
 
-  function cartCard(cardNameMain, cardIdMain) {
+  function cartCard(cardNameMain, cardIdMain, dPrice) {
     addItemMultiple.innerHTML +=
 
       `<div class="add-item" id="dessert-${cardIdMain}">
@@ -59,8 +60,8 @@ const createDessert = async () => {
                 <p class=" product-name">${cardNameMain}</p>
                 <div class="product-num-info">
                   <span class="qty-outer"><span class="qty">1</span>x</span>
-                  <span class="product-price-outer">@ $<span class="product-price">5.50</span></span>
-                  <span class="price-add-outer">$<span class="price-add">5.50</span></span>
+                  <span class="product-price-outer">@ $<span class="product-price">${dPrice}</span></span>
+                  <span class="price-add-outer">$<span class="price-add">${dPrice}</span></span>
                 </div>
               </div>
               <img class="remove" src="assets/images/icon-remove-item.svg" alt="">
@@ -80,6 +81,8 @@ const createDessert = async () => {
     const decrease = card.querySelector(".decrease")
     const increase = card.querySelector(".increase")
     const incQty = card.querySelector(".inc-qty")
+    const cartItems = document.querySelector(".cart-items")
+    console.log(cartItems.innerText)
     // console.log(cardName.innerText)
 
     // add to cart toggle
@@ -87,14 +90,34 @@ const createDessert = async () => {
       atcBtn.classList.add("atcbtn-off")
       productNum.classList.add("productnum-on")
       const cardName = card.querySelector(".dessert-name")
+      const dessertPrice = card.querySelector(".d-price")
       let cardNameMain = cardName.innerText
       let cardIdMain = cardName.id
+      let dPrice = dessertPrice.innerText
       // console.dir(cardName)
-      cartCard(cardNameMain, cardIdMain)
+      cartCard(cardNameMain, cardIdMain, dPrice)
       console.log("button clicked")
-      console.dir(addItemMultiple)
+      // console.dir(addItemMultiple)
       const cartWithItem = document.querySelector(".cart-with-item")
       cartWithItem.classList.remove("cartWithItem-off")
+      const cartEmpty = document.querySelector(".cart-empty")
+      cartEmpty.classList.add("cartEmpty-off")
+      cartItems.innerText++
+      let cardIdProduct = cardName.id
+      // console.log(cardIdProduct)
+      const addItem = document.getElementById(`dessert-${cardIdProduct}`)
+      const pdtQty = addItem.querySelector(".qty")
+      // console.log(incQty.innerText)
+      pdtQty.innerText = incQty.innerText
+
+      // Order Total
+      const OPPrice = addItem.querySelector(".product-price")
+      const tPPrice = document.querySelector(".tp-price")
+      console.log(OPPrice.innerText)
+      console.log(tPPrice.innerText)
+      let orderPrice = parseFloat(tPPrice.innerText) + parseFloat(OPPrice.innerText)
+      tPPrice.innerText = parseFloat(orderPrice)
+
 
     })
 
@@ -109,21 +132,57 @@ const createDessert = async () => {
     decrease.addEventListener("click", (event) => {
       changeNum = changeNum - 1
       incQty.innerText = changeNum
+
+      const cardName = card.querySelector(".dessert-name")
+      let cardIdProduct = cardName.id
+      // console.log(cardIdProduct)
+      const addItem = document.getElementById(`dessert-${cardIdProduct}`)
+      const pdtQty = addItem.querySelector(".qty")
+      console.log(incQty.innerText)
+      pdtQty.innerText = incQty.innerText
+      cartItems.innerText--
+
+      // product price suntract individual
+      const priceAdd = addItem.querySelector(".price-add")
+      const OPPrice = addItem.querySelector(".product-price")
+      let itemPrice = parseInt(priceAdd.innerText)
+      let originalPrice = parseInt(OPPrice.innerText)
+      priceAdd.innerText = itemPrice - originalPrice
+
+      // Order Total
+      const tPPrice = document.querySelector(".tp-price")
+      console.log(OPPrice.innerText)
+      console.log(tPPrice.innerText)
+      let orderPrice = parseFloat(tPPrice.innerText) - parseFloat(OPPrice.innerText)
+      tPPrice.innerText = parseFloat(orderPrice)
+
       // console.log(changeNum)
       if (changeNum <= 0) {
         const cardName = card.querySelector(".dessert-name")
         let cardIdProduct = cardName.id
-        console.log(cardIdProduct)
+        // console.log(cardIdProduct)
         const addItem = document.getElementById(`dessert-${cardIdProduct}`)
         addItem.remove()
         atcBtn.classList.remove("atcbtn-off")
         productNum.classList.remove("productnum-on")
         incQty.innerText = 1
-        console.dir(addItemMultiple)
+        // console.dir(addItemMultiple)
         if (addItemMultiple.childElementCount === 0) {
           const cartWithItem = document.querySelector(".cart-with-item")
           cartWithItem.classList.add("cartWithItem-off")
         }
+        if (addItemMultiple.childElementCount === 0) {
+          const cartEmpty = document.querySelector(".cart-empty")
+          cartEmpty.classList.remove("cartEmpty-off")
+        }
+
+        // product price suntract individual
+        const priceAdd = addItem.querySelector(".price-add")
+        const OPPrice = addItem.querySelector(".product-price")
+        let itemPrice = parseInt(priceAdd.innerText)
+        let originalPrice = parseInt(OPPrice.innerText)
+        priceAdd.innerText = itemPrice - originalPrice
+
       }
     })
 
@@ -132,7 +191,30 @@ const createDessert = async () => {
     increase.addEventListener("click", () => {
       changeNum = changeNum + 1
       incQty.innerText = changeNum
+      cartItems.innerText++
+
+      const cardName = card.querySelector(".dessert-name")
+      let cardIdProduct = cardName.id
+      const addItem = document.getElementById(`dessert-${cardIdProduct}`)
+      const pdtQty = addItem.querySelector(".qty")
+      // console.log(incQty.innerText)
+      pdtQty.innerText = incQty.innerText
+
+      // product price suntract individual
+      const priceAdd = addItem.querySelector(".price-add")
+      const OPPrice = addItem.querySelector(".product-price")
+      let itemPrice = parseFloat(priceAdd.innerText)
+      let originalPrice = parseFloat(OPPrice.innerText)
+      priceAdd.innerText = itemPrice + originalPrice
+
+      // Order Total
+      const tPPrice = document.querySelector(".tp-price")
+      console.log(OPPrice.innerText)
+      console.log(tPPrice.innerText)
+      let orderPrice = parseFloat(tPPrice.innerText) + parseFloat(OPPrice.innerText)
+      tPPrice.innerText = parseFloat(orderPrice)
     })
+
 
   })
 
